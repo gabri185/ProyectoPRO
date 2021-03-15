21ce986197afb552d4f6e6d824dc98c4f47e64eb
Gabriel Domínguez Reyes <gdominguezreyes@danielcastelao.com>
2021-03-15 10:04:22 +0100

    Separando ramas

--- src/Metodos.java
+++ src/Metodos.java
@@ -6,8 +6,6 @@ import javax.swing.JOptionPane;
 
 
 public class Metodos {
-
-
     ArrayList <Integer> listaZapatos= new ArrayList < >();
     /*
     public static Tienda pedirZapatos(String mensaxe){
@@ -24,7 +22,7 @@ public class Metodos {
 */
 
 
-    public static Tienda pedirZapatos(String nomeFich, ArrayList <Tienda> listaZapatos) throws FileNotFoundException{
+    public static Tienda pedirZapatos(){
         String nombre = JOptionPane.showInputDialog("Engade nombre= ");
         String marca = JOptionPane.showInputDialog("Engade marca= ");
         String color = JOptionPane.showInputDialog("Engade color= ");
@@ -33,18 +31,21 @@ public class Metodos {
         int nUnidades = Integer.parseInt(JOptionPane.showInputDialog("Engade numUnidades= "));
 
         Tienda ti= new Tienda(nombre,marca,talla,precio,nUnidades,color);
+       /*
         listaZapatos.add(ti);
         File fi;
         fi = new File(nomeFich+".txt");
         PrintWriter esc =null;
         try {
             esc = new PrintWriter(fi);
-            esc.println(talla);
+            esc.println( "Nombre: " + nombre + "Marca: " + marca + "Color: " + color + "Talla: " + talla + "Precio: " + precio + "nUnidades: " + nUnidades);
+
             esc.println(marca);
             esc.println(nombre);
             esc.println(precio);
             esc.println(color);
             esc.println(nUnidades);
+
 
         }catch (FileNotFoundException ex){
             System.out.println(" erro escribirNumeros "+ ex.toString());
@@ -52,15 +53,31 @@ public class Metodos {
         finally{
             esc.close();
         }
+        */
         return ti;
 
     }
     public static void engadirElemento(ArrayList<Tienda> listaZapatos) throws FileNotFoundException {
-        Tienda za=pedirZapatos ("Ficheiro", listaZapatos);
+        Tienda za=pedirZapatos ();
         listaZapatos.add(za);
     }
+    public static void buscarPorNombre(ArrayList<Tienda>listaZapatos){
+        boolean atopado =false;
+        String nombre = JOptionPane.showInputDialog("libro");
+        for(Tienda al :listaZapatos){
+            if(nombre.equals(al.getNombre())){
+                System.out.println("Este es el zapato =dddd " + al.getNombre() + al.getMarca());
+                atopado=true;
+            }
+        }
+        if(atopado==false){
+            System.out.println("Zapato no encontrado");
 
-
+        }
+    }
+    public static void amosarForEach(ArrayList<Tienda>listaZapatos){
+        for (Tienda al:listaZapatos)
+            System.out.println(al);
+    }
 
 }
-
--- src/proyecto.java
+++ src/proyecto.java
@@ -8,18 +8,22 @@ public class proyecto {
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList <Tienda>lista = new ArrayList<Tienda>();
         String opcion;
+
         do {
-            opcion = JOptionPane.showInputDialog(" *** MENU *** \n1 --> ENGADIR\n2 --> AMOSAR\n3--> SAIR");
+            opcion = JOptionPane.showInputDialog(" *** MENU *** \n1 --> ENGADIR\n2 --> AMOSAR\n3--> BUSCAR\n4--> SAIR");
             switch(opcion){
                 case "1":
                     Metodos.engadirElemento(lista);
                     break;
                 case "2":
-
+                    Metodos.amosarForEach(lista);
                     break;
                 case "3":
+                    Metodos.buscarPorNombre(lista);
+                    break;
+                case "4":
                     System.exit(0);
             }
-        } while (!opcion.equalsIgnoreCase("3"));
+        } while (!opcion.equalsIgnoreCase("4"));
     }
 }
